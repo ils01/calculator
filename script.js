5270833527c1a732a71a30a913def3fd828c7aa9
ilias12 <ishulepov842@gmail.com>
2022-12-20 21:28:48 +0300

    Implement multiple operations, fix equals, round to 3 decimal places

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ function multiply(a, b){
     return a * b;
 }
 function divide(a,b){
-    if(b) return a / b;
+    if(b) {
+        let res = a / b;
+        return Math.round(res * 1000) / 1000;
+    }
     else return "Oopsy!";
 }
 
@@ -25,57 +28,75 @@ let displayText = document.getElementById('displayText');
 let firstNumber = null;
 let secondNumber = null;
 let operator = null;
-let flag = false;
+let flagTypingNum = false;
+
 document.querySelectorAll('.key').forEach(key => {
     key.addEventListener("click", function(){
-        if (firstNumber === null){
+        if(key.id === "ac"){
+            displayText.innerText='0';
+            firstNumber = null;
+            secondNumber = null;
+            operator = null;
+            flagTypingNum = false;
+        }
+        else if (firstNumber === null){
             if(key.classList.contains('number')){
                 if(displayText.innerText === '0'){
                     displayText.innerText = '';
+                    flagTypingNum = true;
                 }
                 displayText.innerText += key.innerText;
             }
-            else if(key.classList.contains('sign') && key.innerText !== "="){
+            else if(key.classList.contains('sign') && key.id !== "equals"){
                 firstNumber = Number(displayText.innerText);
                 operator = key.innerText;
                 displayText.innerText = key.innerText;
+                flagTypingNum = false;
             }
        }
        else if (secondNumber === null && operator !== null){
-            if(key.classList.contains('number')){
-                if(displayText.innerText.length === 1 && !flag){
+            if(key.classList.contains('sign') && !isNaN(displayText.innerText) && flagTypingNum){
+                    secondNumber = Number(displayText.innerText)
+                    displayText.innerText = operate(operator, firstNumber,secondNumber);
+                    firstNumber = Number(displayText.innerText);
+                    secondNumber = null;
+                    flagTypingNum = false;
+                    if(key.id === 'equals') operator = null;
+                    else operator = key.innerText;
+            }
+            else if(key.classList.contains('sign') && isNaN(displayText.innerText) && !flagTypingNum){
+                operator = key.innerText;
+                displayText.innerText = operator;
+            }
+            else if(key.classList.contains('number')){
+                if(!flagTypingNum){
                     displayText.innerText = '';
-                    flag = true;
+                    flagTypingNum = true;
                 }
                 displayText.innerText += key.innerText;
-            }
-            else if(key.classList.contains('sign')){
-                secondNumber = Number(displayText.innerText)
-                displayText.innerText = operate(operator, firstNumber,secondNumber);
-                firstNumber = Number(displayText.innerText);
-                operator = null;
-                secondNumber = null;
-                flag = false;
             }
        }
        else if (secondNumber === null && operator === null){
         if(key.classList.contains('number')){
-            if(!flag){
+            if(!flagTypingNum){
                 displayText.innerText = '';
-                flag = true;
+                flagTypingNum = true;
             }
+            firstNumber = null;
             displayText.innerText += key.innerText;
         }
-        else if(key.classList.contains('sign')){
+        else if(key.classList.contains('sign') && key.id !== "equals"){
+                console.log('here');
+                console.log(key.innerText);
                 operator = key.innerText;
-                secondNumber = Number(displayText.innerText);
-                displayText.innerText = operate(operator,firstNumber,secondNumber);
-                firstNumber = Number(displayText.innerText);
-                operator = null;
+                displayText.innerText = operator;
                 secondNumber = null;
-                flag = false;
+                flagTypingNum = false;
         }
        }
-       
+       console.log("first " + firstNumber)
+       console.log("oper " + operator)
+       console.log("second " + secondNumber)
+       console.log('flagTypingNum ' + flagTypingNum)
     })
 })
